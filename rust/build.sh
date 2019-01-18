91b51583829e0101447ffff49c4aab6b5b47c4a0
neonphog <neonphog@gmail.com>
2019-01-17 17:01:03 -0700

    move so npm/readme work

--- rust/build.sh
+++ rust/build.sh
@@ -2,4 +2,4 @@
 
 echo "building" && \
   cargo build --target wasm32-unknown-unknown --release && \
-  wasm-bindgen target/wasm32-unknown-unknown/release/n_bch_rs.wasm --out-dir ../node/lib --out-name bindgen --nodejs --no-typescript
+  wasm-bindgen target/wasm32-unknown-unknown/release/n_bch_rs.wasm --out-dir ../lib --out-name bindgen --nodejs --no-typescript
